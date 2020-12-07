49cc27d0c7a45a25afb27c70e904cb13c4021ad0
Ev3rdown <35498857+Ev3rdown@users.noreply.github.com>
2020-12-07 13:59:18 +0100

    Ajoutez des fichiers projet.

---
+++ Shared/BusinessData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facturation.Shared
+{
+    public class BusinessData : IBusinessData
+    {
+        public IEnumerable<Facture> Factures { get; }
+        public BusinessData()
+        {
+            Factures = new Facture[]
+            {
+                new Facture("Materiaux","Gerbaud",new DateTime(2020,10,3),new DateTime(2021,01,03),201552),
+                new Facture("Equipement","Arnaud",new DateTime(2020,11,15),new DateTime(2020,12,21),4200,500),
+            };
+
+        }
+    }
+}
---
+++ Shared/Facture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facturation.Shared
+{
+    public class Facture
+    {
+        public string NumeroFacture;
+        public string NomClient;
+        public DateTime DateEmission;
+        public DateTime DateAttenduReglement;
+        public double MontantDu;
+        public double MontantRegle;
+        public Facture(string num,string nomC,DateTime emis,DateTime attendu,double du,double regle=0)
+        {
+            NumeroFacture = num;
+            NomClient = nomC;
+            DateEmission = emis;
+            DateAttenduReglement = attendu;
+            MontantDu = du;
+            MontantRegle = regle;
+        }
+    }
+}
